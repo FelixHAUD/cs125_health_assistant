8fd539cd771461f9ce241753977097d2df0f208a
FelixHAUD <71147033+FelixHAUD@users.noreply.github.com>
2026-02-01 21:38:51 -0800

    Added recommendation endpoint

--- src/backend/indexing/api.js
+++ src/backend/indexing/api.js
@@ -1,5 +1,6 @@
 import express from "express";
 import indexes from "./indexes.json" with { type: "json" };
+import { getRecommendations } from "../recommendation/queryEngine";
 
 const app = express();
 const api = express.Router();
@@ -16,6 +17,22 @@ function getRecipesForMeal(meal) {
 api.get("/breakfast", (req, res) => res.json(getRecipesForMeal("breakfast")));
 api.get("/lunch", (req, res) => res.json(getRecipesForMeal("lunch")));
 api.get("/dinner", (req, res) => res.json(getRecipesForMeal("dinner")));
+
+app.post("./api/recommend", (req, res) => {
+  try {
+    const userContext = req.body;
+
+    const results = getRecommendations(userContext, 10);
+
+    res.json({
+      count: results.length,
+      results
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Recommendation failed" });
+  }
+});
 
 app.use("/api", api);
 
